76424804813baa449c2e438a51790d6d7d9430f9
Albom <o.v.bogomaz1985@gmail.com>
2020-02-14 13:48:31 +0200

    Code formatting

--- test_c/test.c
+++ test_c/test.c
@@ -5,20 +5,18 @@ void igrf_sub_(float* xlat, float* xlong, float* year, float* height,
 
 int main(int argc, char** argv){
 
-	float xlat = 40;
-	float xlong = 288;
-	float year = 2000;
+    float xlat = 40;
+    float xlong = 288;
+    float year = 2000;
     float height = 200;
-	float xl;
-	int icode;
-	float dip;
-	float dec;
+    float xl;
+    int icode;
+    float dip;
+    float dec;
 
-	xl = -1;
-	
-	igrf_sub_(&xlat, &xlong, &year, &height,
+    igrf_sub_(&xlat, &xlong, &year, &height,
                &xl, &icode, &dip, &dec);
-			   
-	printf("icode=%d\nxl=%f\n", icode, xl);
-	return 0;
+
+    printf("icode=%d\nxl=%f\n", icode, xl);
+    return 0;
 }
